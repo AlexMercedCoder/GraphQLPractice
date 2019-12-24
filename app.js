5c423a6717ea9ba445a66b780913f983b563caf1
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-12-23 23:04:17 -0500

    now to begin with the author schema

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema')
 
 
-app.use('/graphql', graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
 
 
 app.listen(4000, () => {console.log('Listening for Requests')})
--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,29 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const {GraphQLObjectType,
+       GraphQLString,
+       GraphQLSchema,
+       GraphQLID} = graphql;
+
+//Dummy Data
+var books = [
+{name: 'book1', genre: 'fantasy', id: '1'},
+{name: 'book2', genre: 'fantasy', id: '2'},
+{name: 'book3', genre: 'fantasy', id: '3'}
+]
+
+var authors = [
+{name: 'author1', age: 55, id: '1'},
+{name: 'author2', age: 44, id: '2'},
+{name: 'author3', age: 22, id: '3'}
+]
 
 // What is a book
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
         name: {type: GraphQLString},
         genre: {type: GraphQLString}
     })
@@ -18,10 +35,12 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: {id: {type: GraphQLString}},
+            args: {id: {type: GraphQLID}},
             resolve(parent, args){
+            console.log(typeof(args.id))
+            return _.find(books, {id: args.id});
 
-                //code to get info from DB
+
             }
         }
     }
